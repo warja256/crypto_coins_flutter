ac012a9662de0130be6c5f66405a442b5ebf0f54
Varya Kush <barbarakushaeva@gmail.com>
2025-02-08 14:23:04 +0300

    Добавлен Talker

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 import 'crypto_coins_list_app.dart';
 import 'repositories/crypto_coins/crypto_coin.dart';
 
 void main() {
+  final talker = TalkerFlutter.init();
+  GetIt.I.registerSingleton(talker);
+  GetIt.I<Talker>().debug('Talker started...');
+
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
       () => CryptoCoinsRepository(dio: Dio()));
   runApp(const CryptoCurrenciesApp());
