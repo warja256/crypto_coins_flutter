34e41dbbf800e70fac0727f935a10456c119cfea
Varya Kush <barbarakushaeva@gmail.com>
2025-03-31 18:44:08 +0300

    сделан дизайн bottomNavigationBar

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -15,13 +15,56 @@ class HomePage extends StatelessWidget {
         FavouriteRoute(),
       ],
       bottomNavigationBuilder: (_, tabsRouter) {
-        return BottomNavigationBar(
-          currentIndex: tabsRouter.activeIndex,
-          onTap: tabsRouter.setActiveIndex,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.money), label: ''),
-            BottomNavigationBarItem(label: '', icon: Icon(Icons.favorite)),
-          ],
+        return SafeArea(
+          bottom: false,
+          child: Container(
+            height: 108,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(255, 32, 32, 32),
+                  blurRadius: 10,
+                  offset: Offset(0, -3),
+                )
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 0.0),
+              child: BottomNavigationBar(
+                selectedIconTheme: Theme.of(context)
+                    .bottomNavigationBarTheme
+                    .selectedIconTheme,
+                unselectedIconTheme: Theme.of(context)
+                    .bottomNavigationBarTheme
+                    .unselectedIconTheme,
+                currentIndex: tabsRouter.activeIndex,
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                onTap: tabsRouter.setActiveIndex,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Image.asset(
+                      tabsRouter.activeIndex == 0
+                          ? 'assets/png/crypto_selected.png'
+                          : 'assets/png/crypto_unselected.png',
+                      width: 40,
+                      height: 40,
+                    ),
+                    label: '', // Добавлен текст
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      tabsRouter.activeIndex == 1
+                          ? Icons.favorite
+                          : Icons.favorite_border_outlined,
+                      size: 35,
+                    ),
+                    label: '', // Добавлен текст
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
     );
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
 
 final darkTheme = ThemeData(
-  appBarTheme: AppBarTheme(
-    backgroundColor: const Color.fromARGB(255, 23, 21, 21),
-    titleTextStyle: TextStyle(
-        color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
-    scrolledUnderElevation: 0.0,
-    iconTheme: IconThemeData(color: Colors.white),
-  ),
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: Colors.yellow,
-  ),
-  scaffoldBackgroundColor: const Color.fromARGB(255, 35, 31, 31),
-  useMaterial3: true,
-  dividerColor: Colors.white10,
-  textTheme: TextTheme(
-    bodyMedium: const TextStyle(
-        color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),
-    bodySmall: TextStyle(
-        // ignore: deprecated_member_use
-        color: Colors.white.withOpacity(0.4),
-        fontSize: 14,
-        fontWeight: FontWeight.w700),
-  ),
-);
+    appBarTheme: AppBarTheme(
+      backgroundColor: const Color.fromARGB(255, 23, 21, 21),
+      titleTextStyle: TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
+      scrolledUnderElevation: 0.0,
+      iconTheme: IconThemeData(color: Colors.white),
+    ),
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.yellow,
+    ),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 35, 31, 31),
+    useMaterial3: true,
+    dividerColor: Colors.white10,
+    textTheme: TextTheme(
+      bodyMedium: const TextStyle(
+          color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),
+      bodySmall: TextStyle(
+          // ignore: deprecated_member_use
+          color: Colors.white.withOpacity(0.4),
+          fontSize: 14,
+          fontWeight: FontWeight.w700),
+    ),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: const Color.fromARGB(255, 49, 45, 45),
+      elevation: 4,
+      selectedIconTheme: IconThemeData(color: Colors.white),
+      unselectedIconTheme: IconThemeData(color: Colors.grey),
+    ));
