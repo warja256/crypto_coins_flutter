32435c3da63c425b361fed9a13c35bcfe48594a8
Varya Kush <barbarakushaeva@gmail.com>
2025-05-10 19:06:08 +0300

    Добавлены эндпоинты для создания и получения транзакций по user_id

---
+++ crypto_coins_backend/lib/routes/transaction_routes.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:crypto_coins_backend/db/db.dart';
+import 'package:crypto_coins_backend/models/transaction.dart';
+import 'package:get_it/get_it.dart';
+import 'package:postgres/postgres.dart';
+import 'package:shelf/shelf.dart';
+import 'package:talker/talker.dart';
+
+final talker = GetIt.I<Talker>();
+
+Future<Response> createTransaction(Request request) async {
+  try {
+    final payload = await request.readAsString();
+    final Map<String, dynamic> transactionMap = jsonDecode(payload);
+    final transaction = Transaction.fromJson(transactionMap);
+
+    final query = Sql.named(
+      'INSERT INTO "Transaction"(transaction_id, user_id, crypto_name, currency, amount, type, total_price, rate, date) VALUES(@transaction_id, @user_id, @crypto_name, @currency, @amount, @type, @total_price, @rate, @date)',
+    );
+
+    await connection.execute(
+      query,
+      parameters: {
+        'transaction_id': transaction.transactionId,
+        'user_id': transaction.userId,
+        'crypto_name': transaction.cryptoName,
+        'currency': transaction.currency,
+        'amount': transaction.amount,
+        'type': transaction.type,
+        'total_price': transaction.totalPrice,
+        'rate': transaction.rate,
+        'date': transaction.date.toIso8601String(),
+      },
+    );
+
+    talker.debug('Transaction created: $transaction.crypto_name');
+    return Response.ok(jsonDecode(transaction.toJson() as String));
+  } catch (e, st) {
+    talker.error('Error creating transaction', e, st);
+    return Response.internalServerError(body: 'Error: $e');
+  }
+}
+
+Future<Response> loadAllTransaction(Request request) async {
+  try {
+    final payload = await request.readAsString();
+    final Map<String, dynamic> data = jsonDecode(payload);
+    final userId = int.parse(data['user_id'].toString());
+    if (userId == null) {
+      talker.warning('user_id not provided');
+      return Response.badRequest(body: 'Missing user_id');
+    }
+
+    final result = await connection.execute(
+      'SELECT * FROM "Transaction" WHERE user_id = @user_id',
+      parameters: {'user_id': userId},
+    );
+
+    if (result.isEmpty) {
+      talker.warning('Transactions not found');
+      return Response.ok(jsonEncode([]));
+    }
+
+    final transactions =
+        result.map((row) => Transaction.fromJson(row.toColumnMap())).toList();
+    talker.debug('✅ Loaded ${transactions.length} transactions');
+    return Response.ok(jsonEncode(transactions));
+  } catch (e, st) {
+    talker.error('Loading transactions error', e, st);
+    return Response.internalServerError(body: 'Error: $e');
+  }
+}
